081261e996af3cac81ac57b7a5c98396878e2430
Arunkv52 <125946001+Arunkv52@users.noreply.github.com>
2025-06-23 18:51:13 +0530

    new

--- src/Components/Carousal.jsx
+++ src/Components/Carousal.jsx
@@ -102,36 +102,7 @@ function Responsive () {
                 </a>
               </div>
             </div>
-            <div className='slide md:pr-2.5'>
-              <div className='SliderItem-1'>
-                <a href='#'>
-                  <div className='slider-content relative top-1/2 left-10 text-white'>
-                    <p className='text-[12px]'>Trucks</p>
-                    <h3 className='text-3xl md:text-[35px] uppercase font-semibold'>
-                      Driving
-                    </h3>
-                    <h3 className='text-3xl md:text-[35px] uppercase font-semibold'>
-                      Progress
-                    </h3>
-                  </div>
-                </a>
-              </div>
-            </div>
-            <div className='slide md:pr-2.5'>
-              <div className='SliderItem-2'>
-                <a href='#'>
-                  <div className='slider-content relative top-1/2 left-10 text-white'>
-                    <p className='text-[12px]'>Cars</p>
-                    <h3 className='text-[35px] uppercase font-semibold'>
-                      Driving
-                    </h3>
-                    <h3 className='text-[35px] uppercase font-semibold'>
-                      Progress
-                    </h3>
-                  </div>
-                </a>
-              </div>
-            </div>       
+                
           </Slider>
         </div>
       </div>
